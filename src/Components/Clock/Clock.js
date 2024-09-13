7b2ffae06ea3f281ca99593ee9993643f1985c9b
Anurag <anuragv8269@gmail.com>
2024-09-13 18:41:27 +0530

    Created Clock component

--- src/Components/Clock/Clock.js
+++ src/Components/Clock/Clock.js
@@ -1,12 +1,28 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 export default function Clock() {
 
-    const [time, setTime] = useState(new Date().getTime());
+    const [time, setTime] = useState(new Date());
+
+    const getTimeFormat = () => {
+        const hours = String(time.getHours()).padStart(2, '0');
+        const minutes = String(time.getMinutes()).padStart(2, '0');
+        const seconds = String(time.getSeconds()).padStart(2, '0');
+
+        return `${hours}:${minutes}:${seconds}`
+    }
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            setTime(new Date());
+        },1000);
+
+        return () => clearInterval(intervalId);
+    },[])
 
     return (
         <div>
-            {time}
+            <h2>{getTimeFormat()}</h2>
         </div>
     )
 }
